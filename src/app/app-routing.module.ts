36c68541d41753d9a6b89b4ce8c96fda4950f9dc
Ajay Karthick <ajaykarthick483@gmail.com>
2021-04-19 22:42:23 +0530

    Added frontend routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeBookComponent } from './recipe/recipe-book/recipe-book.component';
+import { RecipeCreateComponent } from './recipe/recipe-create/recipe-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RecipeBookComponent},
+  {path: 'create', component: RecipeCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
