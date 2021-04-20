311a0f56f0aa7c84f445e782cf3a3103b7013b07
Ajay Karthick <ajaykarthick483@gmail.com>
2021-04-20 19:35:46 +0530

    Implemented logic to handle image file input

--- src/app/recipe/recipe-create/recipe-create.component.ts
+++ src/app/recipe/recipe-create/recipe-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Recipe } from "../recipe.model";
 import { RecipesService } from "../recipes.service";
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-recipe-create',
@@ -12,6 +13,7 @@ export class RecipeCreateComponent implements OnInit {
 
   form: FormGroup;
   ingredients: FormArray;
+  imagePreview: string;
 
   private recipe: Recipe;
   private recipeId: string = null;
@@ -25,7 +27,8 @@ export class RecipeCreateComponent implements OnInit {
       'ingredients': this.formBuilder.array([ this.createIngredient() ]),
       'servings': new FormControl(null, {validators: [Validators.required]}),
       'cookingTime': new FormControl(null, {validators: [Validators.required]}),
-      'calories': new FormControl(null, {validators: [Validators.required]})
+      'calories': new FormControl(null, {validators: [Validators.required]}),
+      'image': new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
     });
   }
 
@@ -41,19 +44,35 @@ export class RecipeCreateComponent implements OnInit {
     this.ingredients.push(this.createIngredient());
   }
 
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({
+      image: file
+    });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+    console.log(file);
+    console.log(this.form);
+  }
+
   onSaveRecipe() {
     if (this.form.invalid) {
       return;
     }
-    this.recipe = {
-      _id: this.recipeId,
-      recipeName: this.form.value.recipeName,
-      style: this.form.value.style,
-      ingredients: this.form.value.ingredients,
-      servings: this.form.value.servings,
-      cookingTime: this.form.value.cookingTime,
-      calories: this.form.value.calories
-    }
-    this.recipesService.addRecipe(this.recipe);
+
+    this.recipesService.addRecipe(this.recipeId,
+      this.form.value.recipeName,
+      this.form.value.style,
+      this.form.value.ingredients,
+      this.form.value.servings,
+      this.form.value.cookingTime,
+      this.form.value.calories,
+      this.form.value.image);
+
+    this.form.reset();
   }
 }
